74739cf22c1de315f66ff15986f4e35d0d810249
jeff <417519208@qq.com>
2016-08-06 01:13:44 +0800

    add method body in the constructor.

--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -9,7 +9,7 @@ public class Singleton {
 	private static Singleton instance;
 	
 	private Singleton(){
-		
+		System.out.println("singlton instance");
 	}
 	
 	public static Singleton getInstance(){
